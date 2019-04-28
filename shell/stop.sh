c7880caa85197022010591c9568a62fe3df16df9
Andrewb <andrewb@innstant.com>
2019-04-28 18:36:39 +0300

    beanstalkd dev2

--- shell/stop.sh
+++ shell/stop.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 docker container stop node-i
 docker container stop innstant4
+docker container stop beanstalkd
 docker rm node-i
 docker rm innstant4
 docker rm composer-i
+docker rm beanstalkd
+printf "\n\n\nFINISH\n"
+printf "Press enter to exit"
+read command #keep alive window
